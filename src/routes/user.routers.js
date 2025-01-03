cd981ad5b47eef0f01194f01d56316f86b50ed49
AkAnikMistry <anikmistry8963@gmail.com>
2025-01-04 01:22:57 +0600

    all user router

--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    loginUser,
+    logoutUser, 
+    registerUser,
+    refreshAccessToken,
+    changePassword,
+    currentUser,
+    updateAccoutDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistry
+ } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,6 +37,13 @@ router.route("/login").post(loginUser)
 //secured route
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changePassword)
+router.route("/current-user").get(verifyJWT,currentUser)
+router.route("/update-account").patch(verifyJWT,updateAccoutDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistry)
 
 
 
